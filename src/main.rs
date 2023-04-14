9556fa25faefe9ac03966e04ebb2eb861145a9a8
Ahmed Aluthman <amo.1980@gmail.com>
2023-04-14 12:51:36 +0300

    health check

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}", &name)
+}
+
+async fn health_check() -> impl Responder{
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
@@ -10,9 +14,7 @@ async fn main() -> Result<(), std::io::Error>{
     HttpServer::new(||{
         App::new()
             .route("/", web::get().to(greet))
+            .route("/healthcheck", web::get().to(health_check))
             .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    }).bind("127.0.0.1:8000")?.run().await
 }
