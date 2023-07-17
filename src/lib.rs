aaa447a9515487bed1a37238457c435b4c6f70ee
amothm80 <amo.1980@gmail.com>
2023-07-17 21:57:59 +0300

    test

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}", &name)
-    ///
+    //
 }
 
 async fn health_check() -> HttpResponse {
