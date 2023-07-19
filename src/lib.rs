b864c698411864b9df156fadc6339f1f67122091
amothm80 <amo.1980@gmail.com>
2023-07-19 16:40:24 +0300

    testing multiline commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {}", &name)
     //
+    //
 }
 
 async fn health_check() -> HttpResponse {
